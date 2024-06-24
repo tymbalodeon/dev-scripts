7cc2828d35ddd0e0912531d3e608ed362694e616
Ben Rosen <benjamin.j.rosen@gmail.com>
2024-06-23 21:56:50 -0400

    feat: update flake

--- flake.nix
+++ flake.nix
@@ -1,18 +1,8 @@
 {
-  description = "Nix configuration";
+  description = "Dev Scripts";
 
   inputs = {
-    home-manager = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/home-manager";
-    };
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    nixpkgs-elm = {
-      url = "github:nixos/nixpkgs/3030f185ba6a4bf4f18b87f345f104e6a6961f34";
-    };
 
     nushell-syntax = {
       type = "github";
@@ -23,13 +13,10 @@
   };
 
   outputs = {
-    home-manager,
     nixpkgs,
-    nixpkgs-darwin,
-    nixpkgs-elm,
     nushell-syntax,
     ...
-  } @ inputs: let
+  }: let
     supportedSystems = [
       "x86_64-darwin"
       "x86_64-linux"
@@ -39,10 +26,7 @@
       nixpkgs.lib.genAttrs supportedSystems
       (system:
         f {
-          pkgs =
-            if system == "x86_64-darwin"
-            then import nixpkgs-darwin {inherit system;}
-            else import nixpkgs {inherit system;};
+          pkgs = import nixpkgs {inherit system;};
         });
   in {
     devShells = forEachSupportedSystem ({pkgs}: {
@@ -52,7 +36,6 @@
           ansible-language-server
           bat
           deadnix
-          delta
           flake-checker
           fzf
           gh
@@ -75,50 +58,15 @@
           yamlfmt
         ];
 
-        shellHook = "pre-commit install --hook-type commit-msg";
+        shellHook = ''
+          bat_config_dir=".config/bat"
+          bat_syntax_dir="''${bat_config_dir}/syntaxes"
+          mkdir -p "''${bat_syntax_dir}"
+          cp ${nushell-syntax}/nushell.sublime-syntax \
+            "''${bat_syntax_dir}/nushell.sublime-syntax"
+          bat cache --build --source "''${bat_config_dir}"
+          pre-commit install --hook-type commit-msg
+        '';
       };
     });
-
-    homeConfigurations = let
-      hosts = ["benrosen" "work"];
-
-      mkHost = hostName: {
-        name = hostName;
-
-        value = home-manager.lib.homeManagerConfiguration {
-          modules = [./darwin/${hostName}/home.nix];
-          pkgs = nixpkgs-darwin.legacyPackages.x86_64-darwin;
-
-          extraSpecialArgs = {
-            inherit nushell-syntax;
-
-            pkgs-elm = import nixpkgs-elm {
-              config.allowUnfree = true;
-              system = "x86_64-darwin";
-            };
-          };
-        };
-      };
-    in
-      builtins.listToAttrs (map mkHost hosts);
-
-    nixosConfigurations = let
-      hosts = ["bumbirich" "ruzia"];
-
-      mkHost = hostName: {
-        name = hostName;
-
-        value = nixpkgs.lib.nixosSystem {
-          modules = [
-            ./nixos/configuration.nix
-            ./nixos/hardware-configurations/${hostName}.nix
-            {networking.hostName = hostName;}
-          ];
-
-          specialArgs = {inherit inputs;};
-        };
-      };
-    in
-      builtins.listToAttrs (map mkHost hosts);
-  };
 }
